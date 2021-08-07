359d962bbedb40dc9af2f89a09fbf4d0f903de3b
Dan Kulinkovich <dankulinkovich@gmail.com>
2021-08-08 00:14:25 +0300

    Initial commit.
    Front-end partly done.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using EasyNote.Model;
+using System.ComponentModel;
+using System.Windows;
+namespace EasyNote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<NoteModel> _notes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _notes = new BindingList<NoteModel>()
+            {
+                new NoteModel(){ Text ="test"},
+                new NoteModel(){ Text ="jusie"}
+            };
+            Note.ItemsSource = _notes;
+            _notes.ListChanged += (object sender, ListChangedEventArgs e) => 
+            {
+                if (e.ListChangedType == ListChangedType.ItemAdded || 
+                e.ListChangedType == ListChangedType.ItemAdded)
+                    MessageBox.Show("action");
+            };
+        }
+
+        private void Note_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Model/NoteModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EasyNote.Model
+{
+    class NoteModel : INotifyPropertyChanged
+    {
+        private bool _isDone;
+        private string _text;
+
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+
+        public string Text
+        {
+            get { return _text; }
+            set 
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set 
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
